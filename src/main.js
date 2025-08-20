510e3d7fa9d5862724ce4fd6de57e5e05d277038
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-08-20 18:54:25 +0300

    Get rid of index.js; small improvements

--- src/main.js
+++ src/main.js
@@ -3,22 +3,55 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// https://github.com/torch2424/wasm-by-example/blob/master/demo-util/
+export const wasmBrowserInstantiate = async (wasmModuleUrl, importObject) => {
+    let response = undefined;
 
-setupCounter(document.querySelector('#counter'))
+    // Check if the browser supports streaming instantiation
+    if (WebAssembly.instantiateStreaming) {
+        // Fetch the module, and instantiate it as it is downloading
+        response = await WebAssembly.instantiateStreaming(
+            fetch(wasmModuleUrl),
+            importObject
+        );
+    } else {
+        // Fallback to using fetch to download the entire module
+        // And then instantiate the module
+        const fetchAndInstantiateTask = async () => {
+            const wasmArrayBuffer = await fetch(wasmModuleUrl).then(response =>
+                response.arrayBuffer()
+            );
+            return WebAssembly.instantiate(wasmArrayBuffer, importObject);
+        };
+
+        response = await fetchAndInstantiateTask();
+    }
+
+    return response;
+};
+
+
+// Using WASM module
+const go = new Go(); // Defined in wasm_exec.js. Don't forget to add this in your index.html.
+
+const runWasmAdd = async () => {
+  // Get the importObject from the go instance.
+  const importObject = go.importObject;
+
+  // Instantiate our wasm module
+  const wasmModule = await wasmBrowserInstantiate("./main.wasm", importObject);
+
+  // Allow the wasm_exec go instance, bootstrap and execute our wasm module
+  go.run(wasmModule.instance);
+
+  // Call the Add function export from wasm, save the result
+  const addResult = wasmModule.instance.exports.add(24, 24);
+
+  // Set the result onto the body
+  document.querySelector('#result').innerHTML = `addResult: ${addResult}`;
+  document.querySelector('#vite-logo').innerHTML = `<img src="${viteLogo}" class="logo" alt="Vite logo" />`;
+  document.querySelector('#js-logo').innerHTML = `<img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />`
+}
+runWasmAdd();
+
+setupCounter(document.querySelector('#counter'));
